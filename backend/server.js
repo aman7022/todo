0b1139a02679ab6014b2aa9f480fedea95db5a62
amn kr <amanroy62077@gmail.com>
2025-04-29 00:55:09 +0530

    pipeline23

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ MongoClient.connect(MONGO_URL, { useUnifiedTopology: true })
   })
   .catch((err) => console.error('MongoDB connection error:', err));
  
-// Routesaidybfuqeirubf abc amcnñnf rtj6kr6u hffuriggj
+// Routesaidybf sumit
 app.get('/todos', async (req, res) => {
   const todos = await todosCollection.find().toArray();
   res.json(todos);
